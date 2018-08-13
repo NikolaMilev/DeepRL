45476c2b83e4cf211b9fba616e3c8e9451fd9013
NM <nikola.n.milev@gmail.com>
2018-08-13 16:32:38 +0200

    Small changes to the structure. Need to retrain the versions without memory and the one with one network and with memory.

--- take_2/dve_mreze_nema_memoriju/agentv2.py
+++ take_2/dve_mreze_nema_memoriju/agentv2.py
@@ -56,7 +56,7 @@ NET_H=105
 NET_W=80
 NET_D=4
 # lr je 2.5e-4 u originalnom radu a 5e-5 u novom, poboljsanom
-LEARNING_RATE = 2.5e-4
+LEARNING_RATE = 2.5e-3
 MOMENTUM = 0.95  
 MIN_GRAD = 0.01
 #LOSS=huberLoss
@@ -105,7 +105,7 @@ def buildNetwork(height, width, depth, numActions):
 	out=Dense(units=numActions, activation="linear")(dense)
 	filtered_out=Multiply()([out, action_in])
 	model=Model(inputs=[state_in, action_in], outputs=filtered_out)
-	opt=RMSprop(lr=LEARNING_RATE, rho=MOMENTUM, epsilon=MIN_GRAD) # , clipvalue=1.0
+	opt=RMSprop(lr=LEARNING_RATE, rho=MOMENTUM, epsilon=MIN_GRAD, clipvalue=1.0) # , clipvalue=1.0
 	model.compile(loss=LOSS, optimizer=opt)
 
 	printmsg("Built and compiled the network!")
--- take_2/plot/scr.sh
+++ take_2/plot/scr.sh
@@ -1,1 +1,2 @@
-cat training_output.txt | grep "Avg\sQ" | sed "s/Avg Q value: /(/g" | sed "s/Avg testing reward: //g" | sed "s/Avg duration: //g" | sed "s/ /,/g" | sed -e "s/^\(.*\)$/\1),/g"
+FILE1=$1
+cat $FILE1 | grep --text "Avg\sQ" | sed "s/Avg Q value: /(/g" | sed "s/Avg testing reward: //g" | sed "s/Avg duration: //g" | sed "s/ /,/g" | sed -e "s/^\(.*\)$/\1),/g"
